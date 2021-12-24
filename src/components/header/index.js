537460451f9896cae7b0834f33a7c5eafdbfe315
Martin Bedoya <macowner@macs-MacBook-Pro.local>
2021-12-24 00:24:23 -0600

    Added useState for mobile link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./header.css";
+import Web from "./web/index";
+import Mobile from "./mobile/index";
+import mobileLogo from "./mobile/assets/apps.png";
 
 function Header() {
-  return <div>This is header</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="logo">MB</div>
+      <div className="menu">
+        {/* menu for large screens */}
+        <div className="web-menu">
+          <Web />
+        </div>
+        {/* menu for small screens */}
+        <div className="mobile-menu">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <img src={mobileLogo} />
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/components/header/mobile/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mobile() {
+  return <div>This is mobile</div>;
+}
+
+export default Mobile;
