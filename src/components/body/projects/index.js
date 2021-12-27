b62ad2588aed3bba66a9e804f5291382d5ccb727
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-12-26 21:48:25 -0600

    Added skills section: skill.js in data has array with skills names and icons. Passed to index.js in skills and styled in skill-card.js

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,29 +1,14 @@
 import React from "react";
+import Separator from "../../common/separator";
 import { ProjectData } from "../../data/projects";
 import ProjectCard from "./project-card";
 import "./projects.css";
-// import blogPic from "./assets/blog-portfolio.png";
-// import fitnessPic from "./assets/fitness-portfolio.png";
-// import gifcastPic from "./assets/gifcast-portfolio.png";
-// import luxPic from "./assets/lux-portfolio.png";
-// import quizPic from "./assets/quiz-portfolio.png";
-// import teamMakerPic from "./assets/team-profile-portfolio.png";
-
-// function Projects() {
-//   return (
-//     <div className="projects">
-//       <label className="section-title">Projects</label>
-//       <div>
-
-//       </div>
-//     </div>
-//   );
-// }
 
 function Projects() {
   const data = ProjectData;
   return (
     <div className="projects">
+      <Separator />
       <label className="section-title">Projects</label>
       <div>
         {data.map((project) => {
---
+++ src/components/data/skills.js
@@ -0,0 +1,56 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "HTML5",
+        icon: <i class="devicon-html5-plain colored"></i>,
+      },
+      {
+        name: "CSS3",
+        icon: <i class="devicon-css3-plain colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "React",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "JQuery",
+        icon: <i class="devicon-jquery-plain colored"></i>,
+      },
+      {
+        name: "HandlebarsJS",
+        icon: <i class="devicon-handlebars-plain colored"></i>,
+      },
+      {
+        name: "Bootstrap",
+        icon: <i class="devicon-bootstrap-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend & Database Management",
+    list: [
+      {
+        name: "NodeJS",
+        icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "Express",
+        icon: <i class="devicon-express-original colored"></i>,
+      },
+      {
+        name: "MySQL",
+        icon: <i class="devicon-mysql-plain colored"></i>,
+      },
+      {
+        name: "MongoDB",
+        icon: <i class="devicon-mongodb-plain colored"></i>,
+      },
+    ],
+  },
+];
