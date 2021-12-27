c7f20ed0a24a2815f090b766ee3d7143eccf5f05
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-12-27 00:12:17 -0600

    Added contact info and download resume option

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./contact.css";
+import Separator from "../../common/separator/index";
+import SocialContact from "../../common/social-contact/index";
 
 function Contact() {
-  return <div>This is contact</div>;
+  return (
+    <div className="contact">
+      <Separator />
+      <label className="section-title">Contact Me</label>
+      <div className="contact-container">
+        <div className="contact-left">
+          <SocialContact />
+        </div>
+        <div className="download">
+          <a
+            download
+            href={require("../../common/assets/martinbedoyadec21resume.pdf")}
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/common/social-contact/index.js
+++ src/components/common/social-contact/index.js
@@ -1,31 +1,50 @@
 import React from "react";
-import githubPic from "./assets/github.png";
-import linkedPic from "./assets/linkedin.png";
-import emailPic from "./assets/email.png";
+import { SocialData } from "../../data/social";
+// import githubPic from "./assets/github.png";
+// import linkedPic from "./assets/linkedin.png";
+// import emailPic from "./assets/email.png";
+
 import "./social-contact.css";
 
 function SocialContact() {
+  const data = SocialData;
   return (
     <div className="social-contact">
-      <a href="https://github.com/josemartin357">
-        <div className="social-icon-div">
-          <img src={githubPic} alt="github" className="social-icon" />
-        </div>
-      </a>
-
-      <a href="https://www.linkedin.com/in/martinbedoya87/">
-        <div className="social-icon-div">
-          <img src={linkedPic} alt="linkedIn" className="social-icon" />
-        </div>
-      </a>
-
-      <a href="mailto:martin.bedoya2021@gmail.com">
-        <div className="social-icon-div">
-          <img src={emailPic} alt="email" className="social-icon" />
-        </div>
-      </a>
+      {data.map((item) => {
+        return (
+          <a href={item.link}>
+            <div className="social-icon-div">
+              <img src={item.icon} className="social-icon" alt="social icons" />
+            </div>
+          </a>
+        );
+      })}
     </div>
   );
 }
+
+// function SocialContact() {
+//   return (
+//     <div className="social-contact">
+//       <a href="https://github.com/josemartin357">
+//         <div className="social-icon-div">
+//           <img src={githubPic} alt="github" className="social-icon" />
+//         </div>
+//       </a>
+
+//       <a href="https://www.linkedin.com/in/martinbedoya87/">
+//         <div className="social-icon-div">
+//           <img src={linkedPic} alt="linkedIn" className="social-icon" />
+//         </div>
+//       </a>
+
+//       <a href="mailto:martin.bedoya2021@gmail.com">
+//         <div className="social-icon-div">
+//           <img src={emailPic} alt="email" className="social-icon" />
+//         </div>
+//       </a>
+//     </div>
+//   );
+// }
 
 export default SocialContact;
