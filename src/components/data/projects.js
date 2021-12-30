ff8488919ab01d26b57ce23b4996881e94d5d93a
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-12-29 22:56:47 -0600

    Updated content for projects desc, changed style of border when an element in navbar is active, added border to images in projects

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,7 +3,7 @@ export const ProjectData = [
     id: 1,
     title: "LUX",
     about:
-      "Node-based application for a car rental business through MVC design pattern",
+      "Node-based application for a car rental business through MVC design. App is designed to show a list of vehicles available and lets user, if signed-in, fill out a reservation form.",
     tags: ["NodeJS", "ExpressJS", "MySQL", "Bulma CSS"],
     demo: "https://lux-rentals.herokuapp.com/",
     github: "https://github.com/josemartin357/LUX",
@@ -13,7 +13,8 @@ export const ProjectData = [
   {
     id: 2,
     title: "TECHIE blog",
-    about: "CMS-style blog where developers can publish posts and comments",
+    about:
+      "CMS-style blog designed for developers to post text content. TECHIE blog lets users post questions, stories or anything they want; and users can also comment to specific posts.",
     tags: ["ExpressJS", "NodeJS", "MySQL", "Bootstrap"],
     demo: "https://techie-blog-mb.herokuapp.com/",
     github: "https://github.com/josemartin357/Tech-notes",
@@ -25,7 +26,7 @@ export const ProjectData = [
     id: 3,
     title: "My Amazing Workout",
     about:
-      "Fitness application that allows users to view, create and track daily workouts; even if they are offline",
+      "Nosql-based fitness tracker application. App allows users to view, create and track daily workouts. Users can also log multiple exercises in a workout on a given day.",
     tags: ["NodeJS", "ExpressJS", "MongoDB", "Heroku"],
     demo: "https://myamazingworkout.herokuapp.com/",
     github: "https://github.com/josemartin357/fitness-tracker",
@@ -34,9 +35,9 @@ export const ProjectData = [
   },
   {
     id: 4,
-    title: "GifCast",
+    title: "GIFcast",
     about:
-      "Application allows you to search the weather forecast an it will return a gif depending on weather conditions",
+      "GIFcast is a weather tool using Open Weather Map API and GIPHY API. App lets users search the weather forecast an it will return a GIF depending on weather conditions, assigned thru a weather rating.",
     tags: [
       "JavaScript",
       "Materialize CSS",
@@ -53,7 +54,7 @@ export const ProjectData = [
     id: 5,
     title: "Code Quiz",
     about:
-      "Timed coding quiz with multiple-choice questions that includes a list of higher scores",
+      "Timed coding quiz to test your HTML and JS knowledge. Quiz includes multiple-choice questions and once all answers are submitted, users can see a list of higher scores.",
     tags: ["HTML", "CSS", "JavaScript"],
     demo: "https://josemartin357.github.io/Code_Quiz/",
     github: "https://github.com/josemartin357/Code_Quiz",
@@ -64,7 +65,7 @@ export const ProjectData = [
     id: 6,
     title: "Team Profile Maker",
     about:
-      "Node.js command-line application that takes information about employees and generates HTML that displays a profile for each person",
+      "Node.js command-line application that takes information about employees and generates HTML that displays a profile for each person.",
     tags: ["NodeJS", "NPM", "Inquirer"],
     demo: "https://github.com/josemartin357/Team-Profile-Generator",
     github: "https://github.com/josemartin357/Team-Profile-Generator",
