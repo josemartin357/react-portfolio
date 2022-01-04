9cc78689fe7ca7bca0a95fb4163754627d3ccb52
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-04 02:15:38 -0600

    Added new projects

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -23,9 +23,19 @@ export const ProjectData = [
     //   "https://github.com/josemartin357/martin-bedoya-portfolio/blob/main/images/Tech-blog-screen.png?raw=true",
     image: require("./assets/blog-portfolio.png"),
   },
-
   {
     id: 3,
+    title: "React Portfolio",
+    about:
+      "You are looking at it! This is a React-based portfolio with components for the main page, about, projects, work, skills and contact sections. Used conditional rendering to display elements in the body section.",
+    tags: ["ReactJS", "EmailJS", "NodeJS", "NPM"],
+    demo: "https://josemartin357.github.io/react-portfolio/",
+    github: "https://github.com/josemartin357/react-portfolio",
+    image: require("./assets/react-portfolio.png"),
+  },
+
+  {
+    id: 4,
     title: "My Amazing Workout",
     about:
       "Nosql-based fitness tracker application. App allows users to view, create and track daily workouts. Users can also log multiple exercises in a workout on a given day.",
@@ -37,7 +47,7 @@ export const ProjectData = [
     image: require("./assets/fitness-portfolio.png"),
   },
   {
-    id: 4,
+    id: 5,
     title: "GIFcast",
     about:
       "GIFcast is a weather tool using Open Weather Map API and GIPHY API. App lets users search the weather forecast an it will return a GIF depending on weather conditions, assigned thru a weather rating.",
@@ -53,9 +63,38 @@ export const ProjectData = [
     //   "https://github.com/josemartin357/martin-bedoya-portfolio/blob/main/images/gifcast-screenshot.png?raw=true",
     image: require("./assets/gifcast-portfolio.png"),
   },
-
   {
-    id: 5,
+    id: 6,
+    title: "Budget Tracker",
+    about:
+      "Progressive Web Application that allows users to add expenses and deposits to their budgets with or without internet connection. If a transaction is added while offline, the app will populate the updated budget with the transaction when brought back online.",
+    tags: ["ExpressJS", "MongoDB", "MongooseJS", "IndexedDB", "Heroku"],
+    demo: "https://expenseslist.herokuapp.com/",
+    github: "https://github.com/josemartin357/budget-tracker",
+    image: require("./assets/budget-portfolio.png"),
+  },
+  {
+    id: 7,
+    title: "My To-Do List",
+    about:
+      "Heroku-deployed application that can be used by anyone to write and save notes. This application also allows the deletion of notes.",
+    tags: ["NodeJS", "ExpressJS", "UUID", "Heroku", "Bootstrap"],
+    demo: "https://my-todolist.herokuapp.com/",
+    github: "https://github.com/josemartin357/Note-taker",
+    image: require("./assets/todo-portfolio.png"),
+  },
+  {
+    id: 8,
+    title: "Work Day Scheduler",
+    about:
+      "Calendar application that allows a user to save events for each hour of the day. App has a current clock and the time blocks in the calendar will change colors depending on whether it is in the past, present or future.",
+    tags: ["HTML", "CSS", "JavaScript", "JQuery"],
+    demo: "https://josemartin357.github.io/Calendar/",
+    github: "https://github.com/josemartin357/Calendar",
+    image: require("./assets/scheduler-portfolio.png"),
+  },
+  {
+    id: 9,
     title: "Code Quiz",
     about:
       "Timed coding quiz to test your HTML and JS knowledge. Quiz includes multiple-choice questions and once all answers are submitted, users can see a list of higher scores.",
@@ -64,16 +103,5 @@ export const ProjectData = [
     github: "https://github.com/josemartin357/Code_Quiz",
     image:
       "https://github.com/josemartin357/martin-bedoya-portfolio/blob/main/images/code-quiz-screen.png?raw=true",
-  },
-  {
-    id: 6,
-    title: "Team Profile Maker",
-    about:
-      "Node.js command-line application that takes information about employees and generates HTML that displays a profile for each person.",
-    tags: ["NodeJS", "NPM", "Inquirer"],
-    demo: "https://github.com/josemartin357/Team-Profile-Generator",
-    github: "https://github.com/josemartin357/Team-Profile-Generator",
-    image:
-      "https://github.com/josemartin357/martin-bedoya-portfolio/blob/main/images/team-profile-maker.png?raw=true",
   },
 ];
